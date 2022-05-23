f1c4c600a7999e0310605b0ab758c4958a63a348
Seremba Patrick <pserembae@gmail.com>
2022-05-23 15:46:57 +0300

    added pages components and images folders and files for the tinder clone app

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import whiteLogo from '../images/tinder_logo_white.png'
+import colorLogo from '../images/color-logo-tinder.png'
+
+const Nav = ({minimal, authToken}) => {
+
+  return (
+    <nav >
+      <div className="logo-container">
+        <img 
+          className='logo' 
+          src={minimal? colorLogo : whiteLogo }
+          alt=""
+         />
+      </div>
+      {!authToken && !minimal && <button className='nav-button'>Log In</button>}
+    </nav>
+  )
+}
+
+export default Nav
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Nav from '../components/Nav';
+
+const Home = () => {
+    const authToken = false;
+    const handleClick = () => {
+        console.log("Clikced")
+    }
+  return (
+      <div className='overlay'>
+        <Nav minimal={false} authToken={authToken}/>
+        <div className='home'>
+            <h1>Swipe Right</h1>
+            <button className='primary-button' onClick={handleClick}>{authToken ? "Signin": "Create Account"}</button>
+        </div>
+      </div>
+  )
+}
+
+export default Home
