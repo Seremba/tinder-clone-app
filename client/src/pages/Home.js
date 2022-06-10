61ea47141510050d23e4c16b6ed09fe9d2e4ae56
Seremba <pserembae@gmail.com>
2022-06-10 18:08:17 +0300

    Add ability for user to log out

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,38 +1,47 @@
 import {useState} from 'react'
 import Nav from '../components/Nav';
 import AuthModal from '../components/AuthModal';
+import {useCookies} from "react-cookie"
 
 const Home = () => {
     const [showModal, setShowModal] = useState(false);
     const [isSignUp, setIsSignUp] = useState(true);
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+    const authToken = cookies.AuthToken
 
-    const authToken = false;
     const handleClick = () => {
-        console.log("Clikced")
-        setShowModal(true);
-        setIsSignUp(true);
-    }
-  return (
-      <div className='overlay'>
-        <Nav 
-        minimal={false} 
-        setShowModal={setShowModal} 
-        showModal={showModal}
-        setIsSignUp={setIsSignUp}
-        />
-        <div className='home'>
-            <h1 className='primary-title'>Swipe Right</h1>
-            <button className='primary-button' onClick={handleClick}>{authToken ? "Signin": "Create Account"}</button>
-        </div>
+      if (authToken) {
+          removeCookie('UserId', cookies.UserId)
+          removeCookie('AuthToken', cookies.AuthToken)
+          window.location.reload()
+          return
+      }
+      setShowModal(true)
+      setIsSignUp(true)
+  }
 
-        { showModal && (
-          <AuthModal 
-            setShowModal={setShowModal} 
-            isSignUp={isSignUp}
-          />
-        )}
-      </div>
-  )
+  return (
+        <div className="overlay">
+            <Nav
+                authToken={authToken}
+                minimal={false}
+                setShowModal={setShowModal}
+                showModal={showModal}
+                setIsSignUp={setIsSignUp}
+            />
+            <div className="home">
+                <h1 className="primary-title">Swipe Right®</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </button>
+
+
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Home
