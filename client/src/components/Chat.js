5cbdd2d9560e0de919b62ae665d76938868f2b0a
Seremba <pserembae@gmail.com>
2022-06-10 09:46:06 +0300

    Work on Chat component to display user image, name, timestamp, and chat message

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
-const Chat = () => {
+const Chat = (descendingOrderMessages) => {
   return (
     <>
       <div className='chat-display'>
-        Chat
+        {descendingOrderMessages.map((message, _index) => {
+          <div key={_index}>
+            <div className="chat-message-header">
+              <div className="image-container">
+                <img src={message.img} alt={`${message.first_name} profile`}/>
+              </div>
+              {message.name}
+            </div>
+            {message.message}
+          </div>
+        })}
       </div>
     </>
   )
